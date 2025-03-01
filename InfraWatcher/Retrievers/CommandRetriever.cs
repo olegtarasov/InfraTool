c9e3f4b6d981054e9a9862755b98cbe6698d53b4
Oleg Tarasov <me@olegtarasov.email>
2025-03-01 17:28:44 +0300

    WorkDir renamed to chdir

--- InfraWatcher/Retrievers/CommandRetriever.cs
+++ InfraWatcher/Retrievers/CommandRetriever.cs
@@ -5,13 +5,13 @@ namespace InfraWatcher.Retrievers;
 public class CommandRetriever : ILinesRetriever
 {
     public required string Command { get; set; }
-    public string WorkDir { get; set; } = string.Empty;
+    public string Chdir { get; set; } = string.Empty;
     public bool IncludeError { get; set; } = false;
 
     public async Task<string[]> GetLines(IDictionary<string, string>? variables)
     {
         var command = new ShellCommand(Command, variables);
-        var (output, _) = await command.RunAsync(WorkDir);
+        var (output, _) = await command.RunAsync(Chdir);
         return IncludeError
             ? output.Select(x => x.Text).ToArray()
             : output.Where(x => x.Type == OutputType.Output).Select(x => x.Text).ToArray();
